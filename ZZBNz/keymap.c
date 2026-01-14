9190116d822bff17e14c617577ae83ae742ae1c6
jpniet <jpniet@icloud.com>
2026-01-14 13:28:27 -0800

    Add custom QMK configs: OSM & Caps WORD Status Indicator; Caps WORD Continue Keycodes

--- ZZBNz/keymap.c
+++ ZZBNz/keymap.c
@@ -1620,3 +1620,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
+
+
+
+// Custom QMK configs
+
+// OSM Armed Status Indicator
+void oneshot_mods_changed_user(uint8_t mods) {
+  STATUS_LED_4((mods & MOD_MASK_SHIFT) != 0);
+}
+
+// Caps WORD Status Indicator
+void caps_word_set_user(bool active) {
+    if (active) {
+        STATUS_LED_4(true);
+    } else {
+        STATUS_LED_4(false);
+    }
+}
+
+// Caps WORD Continue
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+        case KC_AMPR:
+        case KC_SLSH:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
